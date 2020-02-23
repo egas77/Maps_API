e601d6c07cf51b25b8b78e886db1797ac435c63e
Egor Koder <ebedak2003@yandex.ru>
2020-02-23 16:55:04 +0700

    Add scale function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap, QImage
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from ui_main import Ui_MainWindow
+from data.ui.ui_main import Ui_MainWindow
 
 
 class Example(QMainWindow, Ui_MainWindow):
@@ -14,43 +14,56 @@ class Example(QMainWindow, Ui_MainWindow):
         self.geocoder_server = "http://geocode-maps.yandex.ru/1.x/"
         self.geocoder_key = "40d1649f-0493-4b70-98ba-98533de7710b"
 
-        self.initUI()
+        self.toponym = None
+
+        self.params_static_api = {
+            "ll": "83.775671,53.347664",
+            "l": "map",
+            "size": "650,450",
+            "z": 10
+        }
+
+        self.params_geocoder_api = {
+            "apikey": self.geocoder_key,
+            "geocode": None,
+            "format": "json"
+        }
+
+        self.setupUi(self)
+        self.load_image()
 
     def get_address(self, point):
         pass
 
     def get_toponym(self, address):
-        params = {
-            "apikey": self.geocoder_key,
-            "geocode": address,
-            "format": "json"
-        }
-        resp = requests.get(self.geocoder_server, params=params)
+        self.params_geocoder_api["geocode"] = address
+        resp = requests.get(self.geocoder_server, params=self.params_geocoder_api)
         if not resp:
             return
         toponym = resp.json()["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
         return toponym
 
-    def get_image(self, params):
-        resp = requests.get(self.static_api_server, params=params)
+    def get_image(self):
+        resp = requests.get(self.static_api_server, params=self.params_static_api)
         if not resp:
             return
         return resp.content
 
-    def initUI(self):
-        self.setupUi(self)
-
-        toponym = self.get_toponym("Барнаул, Ленина, 61")
-        if toponym:
-            image = self.get_image({
-                "ll": toponym['Point']['pos'].replace(' ', ','),
-                "spn": '0.003,0.003',
-                "l": "map",
-                "size": "650,450"
-            })
+    def load_image(self):
+        image = self.get_image()
+        if image:
             pixmap = QPixmap.fromImage(QImage.fromData(image))
-            pixmap = pixmap.scaled(self.size(), Qt.KeepAspectRatio)
             self.main_map.setPixmap(pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageDown:
+            if self.params_static_api["z"] > 0:
+                self.params_static_api["z"] -= 1
+                self.load_image()
+        elif event.key() == Qt.Key_PageUp:
+            if self.params_static_api["z"] < 18:
+                self.params_static_api["z"] += 1
+                self.load_image()
 
 
 if __name__ == '__main__':
